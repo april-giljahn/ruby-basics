640aeb59c60952562ab14a276c269579acc1ddcf
April Giljahn <giljahn.40@gmail.com>
2022-11-19 20:12:23 -0500

    completed exercises in arrays chapter

---
+++ arrays/exercise_five.rb
@@ -0,0 +1,5 @@
+string = "welcome to america!"
+
+a = string[6] #e
+b = string[11] #a
+c = string[19] #nil
---
+++ arrays/exercise_six.rb
@@ -0,0 +1,6 @@
+names = ['bob', 'joe', 'susan', 'margaret']
+
+names['margaret'] = 'jody'
+
+#arrays are indexed with integers, not strings. do this instead:
+# names[3] = 'jody'

ca4ae66879956b16541bb44c9b81ad84164058ca
April Giljahn <giljahn.40@gmail.com>
2022-12-20 19:34:00 -0500

    finished exercises for ruby book

---
+++ exercises/exercise_ten.rb
@@ -0,0 +1,14 @@
+=begin
+
+1.) can hash values be arrays?
+yes
+
+hash = {names: ['bob', 'joe', 'susan']}
+
+
+2.) can you have an array of hashes
+yes, you can. 
+
+people = [{name: "Tom"}, {name: "Jerry"}, {name: "George"}]
+
+=end
---
+++ exercises/exercise_thirteen.rb
@@ -0,0 +1,3 @@
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+arr.delete_if {|element| element.start_with?('s', 'w')}
